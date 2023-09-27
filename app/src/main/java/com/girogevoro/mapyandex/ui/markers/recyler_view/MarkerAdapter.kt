a183748f0b1ecb00148634121fe614a7669112e0
egorovin <egorovin@mail.ru>
2023-09-27 21:34:31 +0300

    DiffUtil

--- app/src/main/java/com/girogevoro/mapyandex/ui/markers/recyler_view/MarkerAdapter.kt
+++ app/src/main/java/com/girogevoro/mapyandex/ui/markers/recyler_view/MarkerAdapter.kt
@@ -2,9 +2,11 @@ package com.girogevoro.mapyandex.ui.markers.recyler_view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.girogevoro.mapyandex.databinding.FragmentMarkersItemBinding
 import com.girogevoro.mapyandex.domain.entity.MarkerEntity
+
 
 class MarkerAdapter(
     private val updateMarkerListener: (MarkerEntity) -> Unit,
@@ -31,8 +33,21 @@ class MarkerAdapter(
     }
 
     fun setItems(newList: List<MarkerEntity>) {
+        val result = DiffUtil.calculateDiff(DiffUtilCallback(markersList, newList))
         markersList = newList
-        notifyDataSetChanged()
+        result.dispatchUpdatesTo(this)
+    }
+
+    inner class DiffUtilCallback(
+        private var oldItems: List<MarkerEntity>,
+        private var newItems: List<MarkerEntity>,
+    ) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldItems.size
+        override fun getNewListSize(): Int = newItems.size
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldItems[oldItemPosition].id == newItems[newItemPosition].id
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldItems[oldItemPosition] == newItems[newItemPosition]
     }
 }
 
